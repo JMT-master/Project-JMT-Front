360144d8e8f7bd61c8806f3429f2a6e523ca98da
gwanghyeon <agh0625@gmail.com>
2023-08-25 15:15:58 +0900

    3시 15분 안광현

--- src/thema/Curator.jsx
+++ src/thema/Curator.jsx
@@ -50,7 +50,7 @@ const Curator = () => {
         visitTag = visitTag.concat(item.tag.replace(/, /gi, ',').split(','));
         // if(i <= 10){
           return <ImageList key={i} number={i} className='curatorResult-img-li' 
-          data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null}></ImageList>
+          data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null} title={item.title}></ImageList>
         // }
       }));
 
@@ -86,7 +86,7 @@ const Curator = () => {
     if(selectTag.length === 0) {
       setList(copyVisit.items.map((item,i) => {
           return <ImageList key={i} number={i} className='curatorResult-img-li' 
-          data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null}></ImageList>
+          data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null} title={item.title}></ImageList>
       }));
 
       return 0;
@@ -100,7 +100,7 @@ const Curator = () => {
 
       if(tagValue.length !== 0){
         return <ImageList key={i} number={i} className='curatorResult-img-li' 
-        data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null}></ImageList>
+        data={item.repPhoto !== null ? item.repPhoto.photoid.imgpath : null} title={item.title}></ImageList>
       }
     }));
   };
