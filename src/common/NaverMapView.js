5860e491a4e1445eb8cb316d8f7f213e7995d319
eogns-2-dh <eogns2di@naver.com>
2023-08-28 09:45:08 +0900

    0828 09:45 Daehoon

--- src/common/NaverMapView.js
+++ src/common/NaverMapView.js
@@ -1,17 +1,23 @@
-import { Container as MapDiv, NaverMap, Marker } from 'react-naver-maps'
+import { 
+    Container as MapDiv,
+    NaverMap,
+    Marker
+  } from 'react-naver-maps';
+
+
 
 function NaverMapView(props) {
 
     // const {lat,lng} = props.data;
+    //'bg.ol.ts.ctt.lko'
 
     return (
         <MapDiv style={{height: 400,}}>
-            <NaverMap>
+            <NaverMap mapTypes={{arguments: 'bg.ol.ts.ctt.lko'}}>
                 <Marker defaultPosition={{ lat: 37.5666103, lng: 126.9783882 }} />
             </NaverMap>
         </MapDiv>
     )
-
 }
 
 
