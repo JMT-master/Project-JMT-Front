a2a5cad08dfb3555950759dc9e01417aff9de622
gwanghyeon <agh0625@gmail.com>
2023-10-24 18:40:20 +0900

    23년10월24일오후6시40분qna부분up,delete 남았음

--- src/notice/QnABoard.jsx
+++ src/notice/QnABoard.jsx
@@ -7,20 +7,30 @@ import Paging from '../common/Paging';
 import { call } from './../common/ApiService';
 
 
-const Tr = (props) => {
+export const Tr = (props) => {
   const navigate = useNavigate();
-  const { no, Q, category, title, content, createDate } = props.data;
+  // console.log("props : {} ",props);
+  console.log("props.data {} : ",props.data);
+
+  const modDate = new Date(props.data.modDate);
+  // console.log(modDate.getTime);
+
   const deleteItem = props.deleteItem;
+
   const deleteHandler = (e) => {
     deleteItem(props.item);
   }
 
+  const colnumhandler = () => {
+    navigate('/qna/' + props.data.qnaNum)
+  }
+
   return (
     <tr>
-      <td>{Q}</td>
-      <td>{category}</td>
-      <td onClick={() => navigate('/qna/' + no)}>{title}</td>
-      <td>{createDate}</td>
+      <td>{props.data.qnaNum}</td>
+      <td>{props.data.qnaCategory}</td>
+      <td onClick={() => navigate('/qna/' + props.data.qnaNum)}>{props.data.qnaTitle}</td>
+      <td>{props.data.modDate}</td>
       <button type='button' onClick={deleteHandler}>삭제</button>
     </tr>
     
@@ -28,7 +38,6 @@ const Tr = (props) => {
 }
 
 const QnABoard = () => {
-  // const [newQnaData, setNewQnaData] = useState(qnaData);
   const [items, setItems] = useState([]);
   const [currentPage , setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
@@ -42,22 +51,22 @@ const QnABoard = () => {
     call("/qna", "GET", null)
         .then((response) => {
           console.log("response data {}", response.data);
-          console.log("response {}", response);
+          // console.log("response {}", response);
           setItems(response.data);
-        })}, []);
+        })}, [items.length]);
 
   const addItem = (item) => {
-    call("/qna","POST", item)
+    call("/qna/admin/write","POST", item)
         .then((response) => setItems(response.data));
   }
 
   const editItem = (item) => {
-    call("/qna", "PUT", item)
+    call("/qna/admin/"+item.id, "PUT", item)
         .then((response) => setItems(response.data));
   }
 
   const deleteItem = (item) => {
-    call("/qna", "DELETE", item)
+    call("/qna/admin", "DELETE", item)
         .then((response) => setItems(response.data));
   }
 
@@ -101,9 +110,11 @@ const QnABoard = () => {
             </tr>
           </thead>
           <tbody className='cursor'>
-            {currentItems.map((item, index) => {
+            {currentItems
+            .sort((a,b) => b.modDate - a.modDate)
+            .map((item, index) => {
                 return (
-                  <Tr data={item} key={item.id} deleteItem={deleteItem}></Tr>
+                  <Tr data={item} index={index} key={item.id} deleteItem={deleteItem}></Tr>
                 )
             })}
           </tbody>
@@ -115,7 +126,7 @@ const QnABoard = () => {
         onPageChange={handlePageChange}></Paging>
       </div>
       <div>
-        <button type='button' onClick={()=> navigate("/qna/write")}>Q&A 작성하기</button>
+        <button type='button' onClick={()=> navigate("/qna/admin/write")}>Q&A 작성하기</button>
       </div>
     </div>
   );
