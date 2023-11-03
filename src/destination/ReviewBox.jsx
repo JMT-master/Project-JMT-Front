c9435b1064b3ca820df405af66ed3483a28d8ab8
elcrity <elcrity@naver.com>
2023-11-03 16:03:14 +0900

    리뷰 업데이트 반환시 전체 리스트 반환으로 뷰 업데이트하게 수정중

--- src/destination/ReviewBox.jsx
+++ src/destination/ReviewBox.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import {call, setDateFormat} from "../common/ApiService";
+import {BsTrash} from "react-icons/bs";
 
-const ReviewBox = ({ item }) => {
-  const date = new Date(item.date);
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDay();
+const ReviewBox = ({item, deleteHandler}) => {
+  const regDate = setDateFormat(item.regDate);
+  const modDate = setDateFormat(item.modDate);
+  const deleteReview = () =>{
+    deleteHandler(item.reviewIdx);
+  }
+
   return (
-    <div className='reviewBox'>
-      <div className='reviewBox-header'>
-        <div>{item.id}</div>
-        <div>{`${year}/${month}/${day}`}</div>
-      </div>
-      <div className='reviewBox-content'>
-        <img src={item.img} alt="" />
-        <p>{item.content}</p>
-      </div>
-    </div >
+     <div className='reviewBox'>
+       <div className='reviewBox-header'>
+         <div>{item.reviewWriter}</div>
+         <div>{regDate + " : " + modDate}</div>
+       </div>
+       <div className='reviewBox-content'>
+         <p>{item.reviewContent}</p>
+         <img src={item.img} alt=""/>
+       </div>
+       <div className='reviewBox-footer'>
+         <button className="oBtn marginBottomBtn" onClick={deleteReview}><BsTrash/></button>
+       </div>
+     </div>
   )
 }
 
