190acb5c2bf1c909145cbf556af9f6b46ae25fb2
nc199 <nc199@hanmail.net>
2023-11-09 09:29:05 +0900

    231109 merge전 최종

--- src/member/KakaoLogin.jsx
+++ src/member/KakaoLogin.jsx
@@ -6,26 +6,19 @@ import { signin } from './MemberFuc';
 import { AiOutlineLoading } from 'react-icons/ai';
 
 const KakaoLogin = () => {
-  const href = window.location.href;
   let params = new URL(document.URL).searchParams;
   let code = params.get("code");
-
-  console.log('들어옴???');
 
   tokenCall(code);
 
   function tokenCall(code){
-    
-  console.log('들어옴???');
     const url = API_BASE_URL + "/login/auth?code=" + code;
 
     return fetch(url).then((response) => {
       return response.json()
-      // window.location.href("/");
     }).then(result => {
       signin(result);
-    })
-    
+    })    
   }
 
   return (
